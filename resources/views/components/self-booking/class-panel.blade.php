e1c22fc1f6cff45cdd791a9ae4c81ab381f996d3
SimonEvans1969 <simon@nautilus-consulting.co.uk>
2022-05-12 18:08:50 +0100

    Tailwind cleanup

--- resources/views/components/self-booking/class-panel.blade.php
+++ resources/views/components/self-booking/class-panel.blade.php
@@ -1,5 +1,11 @@
-<div class="panel panel-default">
-    <div class="panel-heading">{{ $clubclass->name }}</div>
-    <div class="panel-body">{{ $clubclass->description }}</div>
-    <div class="panel-footer">{{ (isset($classAttributes->MembersOnly) && ($classAttributes->MembersOnly == 'Y')) ? 'Members only' : 'Click to book'}}</div>
+<div>
+    <div class="px-5 py-3">
+        <h3 class="text-gray-700 uppercase">{{ $clubclass->name }}</h3>
+        <span class="text-gray-500">{{ $clubclass->description }}</span>
+        <form action="cart.store" method="POST" enctype="multipart/form-data">
+            @csrf
+            <input type="hidden" value="{{ $clubclass->id }}" name="id">
+            <button class="text-white bg-blue-800 rounded">Click to book</button>
+        </form>
+    </div>
 </div>
